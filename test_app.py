549ac267b2e53d08f47e4156d3d3c1574d08c8a2
Nayil-Ahmed-Siddique <114480785+Nayil-Ahmed-Siddique@users.noreply.github.com>
2026-04-19 14:57:50 +0300

    Add test for prediction endpoint

--- test_app.py
+++ test_app.py
@@ -6,3 +6,19 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
+def test_predict():
+    payload = {
+        "Pregnancies": 6,
+        "Glucose": 148,
+        "BloodPressure": 72,
+        "SkinThickness": 35,
+        "Insulin": 125,
+        "BMI": 33.6,
+        "DiabetesPedigreeFunction": 0.627,
+        "Age": 50
+    }
+
+    response = client.post("/predict", json=payload)
+
+    assert response.status_code == 200
+    assert "prediction" in response.json()
